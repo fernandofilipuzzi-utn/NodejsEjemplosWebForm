e8ac980ac372fce59884fed52c8122e652db379e
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-11-11 16:55:30 -0300

    agregando formulario

--- ejemplo1/express_ws_routing.js
+++ ejemplo1/express_ws_routing.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var path = require("path");
+const cors = require('cors');
 
 var app = express();
+app.use(cors()); // Permitir todas las solicitudes CORS
 
 var staticPath = path.resolve(__dirname, "public");
 app.use(express.static(staticPath));
@@ -11,9 +13,10 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/form2", function (req, res) {
-    var texto=req.query.numero;
-    console.log(texto)
+app.get("/form", function (req, res) {
+    var numero=req.query.numero;
+    var texto=req.query.texto;
+    console.log(numero +'-'+texto)
 });
 
 app.listen(3000, function(){
